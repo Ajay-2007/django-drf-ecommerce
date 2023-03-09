1afafe609c99c02e730baccc9a796ff43b6c5839
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-09 08:01:59 +0530

    testing product image factory

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -122,4 +122,4 @@ class ProductImage(models.Model):
         return super(ProductImage, self).save(*args, **kwargs)
 
     def __str__(self):
-        return str(self.url)
+        return str(self.order)
--- drfecommerce/drfecommerce/tests/conftest.py
+++ drfecommerce/drfecommerce/tests/conftest.py
@@ -4,12 +4,13 @@ from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
 
-from .factories import CategoryFactory, BrandFactory, ProductFactory, ProductLineFactory
+from .factories import CategoryFactory, BrandFactory, ProductFactory, ProductLineFactory, ProductImageFactory
 
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
 register(ProductLineFactory) # to access ProductLineFactory we have to call product_line_factory
+register(ProductImageFactory)
 
 
 # pytest fixtures
