9321762f6111dcb4b72163a2f26f8c43b7953529
Ajay-2007 <iit2017040@iiita.ac.in>
2023-03-11 07:11:56 +0530

    build product type table iteration

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -42,7 +42,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     category = TreeForeignKey('Category', null=True, blank=True, on_delete=models.PROTECT)
-    # product_type = models.ForeignKey("ProductType", on_delete=models.PROTECT)
+    product_type = models.ForeignKey("ProductType", on_delete=models.PROTECT, related_name="product_type")
 
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(
@@ -131,6 +131,7 @@ class ProductLine(models.Model):
     #     through="ProductLineAttributeValue",
     #     related_name="product_line_attribute_value",
     # )
+    product_type = models.ForeignKey("ProductType", on_delete=models.PROTECT, related_name="product_line_type")
     created_at = models.DateTimeField(
         auto_now_add=True,
         editable=False,
@@ -185,6 +186,7 @@ class ProductImage(models.Model):
 class ProductType(models.Model):
 
     name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
 
     attribute = models.ManyToManyField(
         Attribute,
